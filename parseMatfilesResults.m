b8cd9f44a5f81564df6d7fca37599f25950aaa5b
Abraham@Nehru <aby.vinod@gmail.com>
2020-06-16 16:43:56 -0500

    Parser for matfiles

---
+++ parseMatfilesResults.m
@@ -0,0 +1,14 @@
+list_of_all_matfiles = dir('matfiles/results/*.mat');
+T = [];
+for index = 1:length(list_of_all_matfiles)
+    filename = strcat('matfiles/results/', list_of_all_matfiles(index).name);
+    data = load(filename, 'prob_thresh', 'max_reach_prob', ...
+        'elapsed_time_cco', 'ratio_volume');
+    data.prob_thresh = sprintf('%1.2f', data.prob_thresh);
+    data.max_reach_prob = sprintf('%1.2f', data.max_reach_prob);
+    data.elapsed_time_cco = sprintf('%1.2f', data.elapsed_time_cco);
+    T = [T;struct2table(data)];
+end
+T.Properties.RowNames = {list_of_all_matfiles.name};
+writetable(T, 'matfiles/results/table.txt', 'Delimiter', ',', ...
+    'WriteRowNames', true);
