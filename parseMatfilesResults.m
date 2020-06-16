e612a111070b7fff7480ab19850b1df1e863b7de
Abraham@ARCH2020 <aby.vinod@gmail.com>
2020-06-16 21:45:14 +0000

    Rerun of the code with homogenized variables

--- parseMatfilesResults.m
+++ parseMatfilesResults.m
@@ -4,9 +4,11 @@ for index = 1:length(list_of_all_matfiles)
     filename = strcat('matfiles/results/', list_of_all_matfiles(index).name);
     data = load(filename, 'prob_thresh', 'max_reach_prob', ...
         'elapsed_time_cco', 'ratio_volume');
-    data.prob_thresh = sprintf('%1.2f', data.prob_thresh);
-    data.max_reach_prob = sprintf('%1.2f', data.max_reach_prob);
-    data.elapsed_time_cco = sprintf('%1.2f', data.elapsed_time_cco);
+%     data.prob_thresh = sprintf('%1.2f', data.prob_thresh);
+%     data.max_reach_prob = sprintf('%1.2f', data.max_reach_prob);
+%     data.elapsed_time_cco = sprintf('%5.2f', data.elapsed_time_cco);
+%     data.ratio_volume = sprintf('%1.2f', data.ratio_volume);
+    disp(data)
     T = [T;struct2table(data)];
 end
 T.Properties.RowNames = {list_of_all_matfiles.name};
